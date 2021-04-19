b8028d8c152bf950667654668f57904651c7d120
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2021-04-19 14:43:42 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,30 +1,3 @@
-class User(flask_login.UserMixin):
-    pass
-
-
-@login_manager.user_loader
-def user_loader(name):
-    if name not in data:
-        return
-
-    user = User()
-    user.id = name
-    return user
-
-
-@login_manager.request_loader
-def request_loader(request):
-    name = request.form.get('name')
-    if name not in data:
-        return
-
-    user = User()
-    user.id = name
-
-    user.is_authenticated = request.form['password'] == data[name]['password']
-
-    return user
-
 
 class Markets(db.Model):
     id = db.Column(db.Integer, primary_key=True)
